dda003aba1609ace97634f9c4cc15158b3ab39ed
vagrant <vagrant@homestead>
2019-05-27 09:29:49 +0000

    用户注册页面

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -4,6 +4,6 @@
         <h1>hello laravel1</h1>
         <p class="lead">你现在所看到的是的 <a href="">laravel入门教程的实力项目主页</a> </p>
         <p>一切从这里开始</p>
-        <p><a class="btn btn-lg btn-success" href="#">现在注册</a></p>
+        <p><a class="btn btn-lg btn-success" href="{{route('signup')}}">现在注册</a></p>
     </div>
 @stop
