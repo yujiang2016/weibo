b7aa938986ac72cb5c1a7db4496195fa53b6a2b9
vagrant <vagrant@homestead>
2019-05-29 08:28:57 +0000

    用户微博列表

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
         // $this->call(UsersTableSeeder::class);
         Model::unguard();
         $this->call(UserTableSeeder::class);
+        $this->call(StatusesTableSeeder::class);
         Model::reguard();
     }
 }
