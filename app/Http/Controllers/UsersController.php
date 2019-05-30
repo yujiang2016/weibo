b344a9641bfbfcb67cc90153fcf7b62f622ac466
vagrant <vagrant@homestead>
2019-05-30 01:37:15 +0000

    关注和粉丝列表页面

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -102,4 +102,37 @@ class UsersController extends Controller
             $message->from($from, $name)->to($to)->subject($subject);
         });
     }
+    //显示用户的关注人列表
+    public function followings(User $user){
+        $users=$user->followings()->paginate(30);
+        $title=$user->name.'关注的人';
+        return view('users.show_follow',compact('users','title'));
+    }
+    //显示用户的粉丝列表
+    public  function followers(User $user){
+        $users=$user->followers()->paginate(30);
+        $title=$user->name.'关注的人';
+        return view('users.show_follow',compact('users','title'));
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
