c201de5087e1a7323b544c24ed813d553a9d3ea0
vagrant <vagrant@homestead>
2019-05-30 01:53:09 +0000

    关注按钮

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -8,7 +8,11 @@
                 <div class="offset-md-2 col-md-8">
                     <section class="user_info">
                         @include('shared._user_info', ['user' => $user])
+
                     </section>
+                    @if (Auth::check())
+                        @include('users._follow_form')
+                    @endif
                     <section class="stats mt-2">
                         @include('shared._stats', ['user' => $user])
                     </section>
